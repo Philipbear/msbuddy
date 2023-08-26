cf42f7f09107e82d6180d870896aced3ef7a2dd2
Shipei Xing <philipxsp@hotmail.com>
2023-08-25 19:37:37 -0700

    readme & docs

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'msbuddy'
+copyright = '2023, Shipei Xing'
+author = 'Shipei Xing'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
+    'myst_parser'
+]
+
+master_doc = 'index'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
