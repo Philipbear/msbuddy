1bbac682f482df185ff97d76c56d4813a5311c25
Philipbear <philipxsp@hotmail.com>
2023-10-15 22:52:45 -0700

    test

--- test.py
+++ test.py
@@ -1,5 +1,26 @@
 from msbuddy import Msbuddy, MsbuddyConfig
 
+# instantiate a MsbuddyConfig object
+msb_config = MsbuddyConfig(# highly recommended to specify
+                           ms_instr='orbitrap',  # supported: "qtof", "orbitrap" and "fticr"
+                           # whether to consider halogen atoms FClBrI
+                           halogen=False)
+
+# instantiate a Msbuddy object
+msb_engine = Msbuddy(msb_config)
+
+# you can load multiple USIs at once
+msb_engine.load_usi(['mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00003740036',
+                     'mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00003740037'])
+
+# annotate molecular formula
+msb_engine.annotate_formula()
+
+# retrieve the annotation result summary
+result = msb_engine.get_summary()
+
+
+
 if __name__ == '__main__':
     # instantiate a MsbuddyConfig object
     msb_config = MsbuddyConfig(ms_instr='orbitrap', # supported: "qtof", "orbitrap" and "fticr"
@@ -19,6 +40,3 @@ if __name__ == '__main__':
 
     # retrieve the annotation result summary
     result = msb_engine.get_summary()
-
-    # print the result
-    print(result)
