f6e4e9ef12d00cc29f0feeb56b65e04a18d72c20
Philipbear <philipxsp@hotmail.com>
2024-10-26 09:40:23 -0700

    license update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache 2.0 License",
         "Operating System :: OS Independent",
     ],
     packages=["msbuddy"],
