410c325b9c2592680b42edf9f04dc5e19cd920f6
Philipbear <philipxsp@hotmail.com>
2025-01-03 16:22:10 -0800

    0.3.9

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     packages=["msbuddy"],
